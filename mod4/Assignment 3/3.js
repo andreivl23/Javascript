8cd59e8ffbdacbf7fae65e4b2d915a2fef7ff369
Andrei <anvlsnko@gmail.com>
2023-11-28 19:44:24 +0200

    Added ass3

---
+++ mod4/Assignment 3/3.js
@@ -0,0 +1,49 @@
+'use strict';
+const target = document.getElementById('results')
+async function getShow() {
+
+  const searchQuery = document.querySelector('#query').value;
+
+  try {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${searchQuery}`);
+    const json = await response.json();
+    return json;} catch (error) {console.log('Error: ', error);
+  }
+}
+
+async function getArticles() {
+  target.innerHTML = '';
+  const json = await getShow();
+  for (let show of json) {
+    const article = document.createElement('article');
+    const header = document.createElement('h2');
+    const url = document.createElement('a');
+    const img = document.createElement('img');
+    const summary = document.createElement('div')
+    header.textContent = `${show.show.name}`;
+    url.href = `${show.show.url}`; url.textContent = 'Link to show'; url.target = '_blank';
+    img.src = `${show.show.image?.medium}`; img.alt = `${show.show.name}`;
+    summary.innerHTML = show.show.summary;
+    article.appendChild(header); article.appendChild(img); article.appendChild(url); article.appendChild(summary)
+
+    target.appendChild(article);
+  }
+
+}
+
+
+document.querySelector("#butt").addEventListener('click', async function() {
+  getShow();
+  getArticles();
+});
+
+document.querySelector("#query").addEventListener('keydown', function(event) {
+  // Check if the pressed key is Enter (key code 13)
+  if (event.keyCode === 13) {
+    // Prevent the default behavior of the Enter key (form submission)
+    event.preventDefault();
+    // Trigger the search function
+    getArticles();
+  }
+});
+
